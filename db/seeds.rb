6925b873031541fb95f23b3335baa82331e674e4
Santiago Ferrari <113905797+sferrari91@users.noreply.github.com>
2022-11-16 16:30:30 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'ugly & expensive Flat Berlin',
+  address: 'Friedriechstr. 4, Berlin',
+  description: 'A lovely, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Nice & cool Flat Berlin',
+  address: 'Knorrpromenade. 4, Berlin',
+  description: 'A  large kitchen and a beautiful plants',
+  price_per_night: 120,
+  number_of_guests: 2
+)
+
+
+Flat.create!(
+  name: 'Nice Flat Buenos Aires',
+  address: 'Gorriti 4658, Buenos Aires',
+  description: 'Nice flat with  cool views',
+  price_per_night: 40,
+  number_of_guests: 2
+)
